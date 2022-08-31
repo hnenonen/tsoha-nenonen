e368a5291ca8a8df0e6cc768830e7dab5f7b70da
Heikki Nenonen <heikki.nenonen@iki.fi>
2022-08-31 10:34:52 +0300

    updated schema.sql according to database sketch

--- schema.sql
+++ schema.sql
@@ -1,15 +1,18 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
-    password TEXT
+    password TEXT,
+    admin INTEGER
 );
 
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     taskname TEXT,
     content TEXT,
+    work_time INTEGER,
     task_state TEXT,
     user_id INTEGER REFERENCES users,
+    worker_id INTEGER REFERENCES users,
     time TIMESTAMP
 );
 
@@ -17,6 +20,7 @@ CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     task_id INTEGER REFERENCES tasks,
     comment TEXT,
+    time TIMESTAMP,
     user_id INTEGER REFERENCES users
 );
 
@@ -31,3 +35,22 @@ CREATE TABLE team_info (
     teamname TEXT UNIQUE,
     password TEXT
 );
+
+CREATE TABLE direct_message (
+    id SERIAL PRIMARY KEY,
+    sender_id INTEGER REFERENCES users,
+    receiver_id INTEGER REFERENCES users,
+    message TEXT,
+    time TIMESTAMP
+);
+
+CREATE TABLE user_info (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    name TEXT,
+    age INTEGER,
+    motto TEXT,
+    content TEXT,
+    updated TIMESTAMP
+); 
+
