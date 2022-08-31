d2f03b42cf26485cc9aaea7f7e661088a77f2bb9
Heikki Nenonen <heikki.nenonen@iki.fi>
2022-08-31 14:24:48 +0300

    lisätty kommentteihin lähetysaika ja esitetään kommentit aikajärjestyksessä

--- comments.py
+++ comments.py
@@ -2,7 +2,7 @@ from db import db
 import tasks, users
 
 def get_list(task_id):
-    sql = "SELECT DISTINCT C.comment, U.username FROM comments C, tasks T, users U WHERE C.task_id=:task_id AND U.id = C.user_id"
+    sql = "SELECT DISTINCT C.comment, U.username, C.time FROM comments C, tasks T, users U WHERE C.task_id=:task_id AND U.id = C.user_id ORDER BY C.time DESC"
     result = db.session.execute(sql, {"task_id": task_id})
     return result.fetchall()
 
@@ -10,7 +10,7 @@ def comment(task_id, comment):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO comments (task_id, comment, user_id) VALUES (:task_id, :comment, :user_id)"
+    sql = "INSERT INTO comments (task_id, comment, user_id, time) VALUES (:task_id, :comment, :user_id, NOW())"
     result = db.session.execute(sql, {"task_id": task_id, "comment":comment, "user_id":user_id})
     db.session.commit()
     return True
